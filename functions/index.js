08bb03ebe9cb6a76c3adac1e8db8bd91828aaf4c
-=EL=- <IT0671581471@gmail.com>
2020-10-31 20:56:58 +0200

    add in keyup #condition

--- functions/index.js
+++ functions/index.js
@@ -1,46 +1,48 @@
-document.querySelectorAll("input[name], #formula").forEach(function (input) {
-  input.addEventListener("keyup", function () {
-    var a1 = document.querySelector('[name="a1"]');
-    var b1 = document.querySelector('[name="b1"]');
-    var a2 = document.querySelector('[name="a2"]');
-    var b2 = document.querySelector('[name="b2"]');
-    var formula = document.querySelector("#formula");
-    var condition = document.querySelector("#condition");
-    var result = document.querySelector("#result");
+document
+  .querySelectorAll("input[name], #formula, #condition")
+  .forEach(function (input) {
+    input.addEventListener("keyup", function () {
+      var a1 = document.querySelector('[name="a1"]');
+      var b1 = document.querySelector('[name="b1"]');
+      var a2 = document.querySelector('[name="a2"]');
+      var b2 = document.querySelector('[name="b2"]');
+      var formula = document.querySelector("#formula");
+      var condition = document.querySelector("#condition");
+      var result = document.querySelector("#result");
 
-    try {
-      var calculator = new Function(
-        "a1, b1, a2, b2",
-        "return " + formula.value + ";"
-      );
+      try {
+        var calculator = new Function(
+          "a1, b1, a2, b2",
+          "return " + formula.value + ";"
+        );
 
-      var compare = new Function(
-        "a1, b1, a2, b2",
-        "return " + condition.value + ";"
-      );
+        var compare = new Function(
+          "a1, b1, a2, b2",
+          "return " + condition.value + ";"
+        );
 
-      result.value = calculator(
-        Number(a1.value),
-        Number(b1.value),
-        Number(a2.value),
-        Number(b2.value)
-      );
-
-      if (
-        compare(
+        result.value = calculator(
           Number(a1.value),
           Number(b1.value),
           Number(a2.value),
           Number(b2.value)
-        ) == true
-      ) {
-        result.style.background = "#B6D7A8";
-      } else {
-        result.style.background = "#ffffff";
+        );
+
+        if (
+          compare(
+            Number(a1.value),
+            Number(b1.value),
+            Number(a2.value),
+            Number(b2.value)
+          )
+        ) {
+          result.style.background = "#B6D7A8";
+        } else {
+          result.style.background = "#ffffff";
+        }
+      } catch (error) {
+        result.value = "#ERROR";
+        console.error(error.message);
       }
-    } catch (error) {
-      result.value = "#ERROR";
-      console.error(error.message);
-    }
+    });
   });
-});
